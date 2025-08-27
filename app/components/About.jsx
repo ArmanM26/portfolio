f7ae6c936fca475cc295283de3ce718de44721f6
ArmanM26 <armanmkrtchyan260612@gmail.com>
2025-08-28 00:26:15 +0400

    feat: implement main page structure and component layout

---
+++ app/components/About.jsx
@@ -0,0 +1,46 @@
+import { assets,infoList, toolsData } from '@/assets/assets'
+import React from 'react'
+import Image from 'next/image'
+
+const About = () => {
+  return (
+    <div id="about" className="w-full px-[12%] py-10 scroll-mt-10">
+        <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
+        <h2 className='text-center text-5xl font-Ovo'>About me</h2>
+        <div className='flex w-full flex-col lg:flex-row items-center gap-20 mt-20'>
+            <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+                <Image src={assets.user_image} alt='' className='w-full rounded-3xl'/>
+            </div>
+            <div className='flex-1'>
+                <p className='mb-10 max-w-2xl font-Ovo'>
+                     I am a Frontend Developer skilled in JS,React,Next.js,HTML,CSS,Tailwind CSS and
+                     Node.js with practical experience from hands-on projects.
+                    Seeking a front-end role or internship to apply technical
+                    skills and grow as a web developer.
+                </p>
+                <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+                    {infoList.map(({icon,iconDark,title,description},index)=>(
+                        <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 hover:shadow-[4px_4px_0_#000]
+                        duration-500' key={index}>
+                            <Image src={icon} alt={title} className='w-7 mt-3'/>
+                            <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
+                            <p className='text-gray-600 text-sm'>{description}</p>
+                        </li>
+                    ))}
+                </ul>
+
+                <h4 className='my-6 text-gray-700 font-Ovo'>Tools I use</h4>
+                <ul className='flex items-center gap-3 sm:gap-5'>
+                    {toolsData.map((tool,index)=>(
+                        <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500' key={index}>
+                            <Image src={tool} alt='Tool' className='w-5 sm:w-7' />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
-import Image from "next/image";
-
+"use client";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <About />
+    </div>
+  );
 }
