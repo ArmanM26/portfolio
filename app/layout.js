fdba2fa7a6c0c1434eeec51fd42769fd1f146823
ArmanM26 <armanmkrtchyan260612@gmail.com>
2025-08-28 14:30:21 +0400

    feat: add Work section

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,56 @@
+// import { Outfit, Ovo, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+
+// const outfit = Outfit({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600", "700"],
+// });
+
+// const ovo = Ovo({
+//   subsets: ["latin"],
+//   weight: ["400"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// export const metadata = {
+//   title: "Portfolio - Arman",
+//   description: "",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en" className="scroll-smooth">
+//       <body
+//         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
 import { Outfit, Ovo, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const outfit = Outfit({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
+  variable: "--font-outfit",
 });
 
 const ovo = Ovo({
   subsets: ["latin"],
   weight: ["400"],
+  variable: "--font-ovo",
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
+  variable: "--font-geist-mono",
 });
 
 export const metadata = {
@@ -25,7 +62,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+        className={`${outfit.variable} ${ovo.variable} ${geistMono.variable} antialiased leading-8 overflow-x-hidden`}
       >
         {children}
       </body>
